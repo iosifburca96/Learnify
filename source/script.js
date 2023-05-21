45f90317fd211d019516e459b4323db95fbb8a79
Iosif Burca <iosifburca1@gmail.com>
2023-05-21 23:46:33 +0300

    21.05

--- source/script.js
+++ source/script.js
@@ -1,14 +1,26 @@
-let switchBtn = document.getElementById("theme-btn");
-let body = document.querySelector("body");
+let ropeTrigger = document.querySelector('.switch img');
+let sun = document.querySelector('.sun');
+let moon = document.querySelector('.moon');
+let darkLogo = document.querySelector('.logo-wrapper .dark');
+let lightLogo = document.querySelector('.logo-wrapper .light');
+let body = document.body;
 
-function changeBackground() {
-    if(body.style.backgroundColor == 'white') {
-        body.style.backgroundColor = 'darkblue';
-        body.style.color = 'yellow';
-    } else {
-        body.style.backgroundColor = 'white';
-        body.style.color = 'black';
-    } 
-};
+function changeTheme() {
+    if(body.style.backgroundColor != 'rgb(6, 7, 54)') {
+        //change to dark theme;
+        sun.style.visibility = 'hidden';
+        moon.style.visibility = 'visible';
+        darkLogo.style.display = 'none';
+        lightLogo.style.display = 'inline';
+        body.style.backgroundColor = 'rgb(6, 7, 54)';
+    } else if (body.style.backgroundColor != 'rgb(59,141,212)') {
+        //change to light theme;
+        moon.style.visibility = 'hidden';
+        sun.style.visibility = 'visible';
+        lightLogo.style.display = 'none';
+        darkLogo.style.display = 'inline';
+        body.style.backgroundColor = 'rgb(59,141,212)';
+    }
+}
 
-switchBtn.addEventListener("click", changeBackground);
+ropeTrigger.addEventListener('click', changeTheme);
